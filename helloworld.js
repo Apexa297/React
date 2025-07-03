27bb005fd65e96498ef52882468b0c044e65c4c7
Apexa297 <apexasolanki297@gmail.com>
2025-07-03 22:44:44 +0530

    episode 1

---
+++ helloworld.js
@@ -0,0 +1,16 @@
+/*const heading = document.createElement("h1");
+heading.innerHTML = "hello world from javascript";
+
+const root = document.getElementById("root");
+root.appendChild(heading);*/
+
+const parent = React.createElement(
+  'div',
+  { id: "parent" },
+  [React.createElement("div",{ id: "child" },[
+        React.createElement("h1", {}, "I am h1 tag"),
+        React.createElement("h1", {}, "I am h1 tag")
+      ]
+    )
+  ]
+);
